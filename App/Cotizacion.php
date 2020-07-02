9cde06138466813112929f2cc57f7bc50b6799f6
Anibal Moradel <daniel.lmp@outlook.com>
2020-07-02 06:37:36 +0000

    Primer Commit

---
+++ App/Cotizacion.php
@@ -0,0 +1,8 @@
+<?php
+
+class Cotizacion
+{
+    protected $guarded  = [];
+}
+
+
---
+++ plantillaPdf.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporte</title>
+</head>
+
+<body>
+    <table border="1">
+        <tr>
+            <td>Nombre</td>
+            <td>Apellido</td>
+        </tr>
+        <tr>
+            <td>Anibal</td>
+            <td>Moradel</td>
+        </tr>
+    </table>
+</body>
+
+</html>
